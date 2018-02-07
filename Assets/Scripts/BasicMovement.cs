9e0eff3f834fa8185a75803f2419396514025d8d
Alec McClure <sabotaione@gmail.com>
2018-02-07 16:44:53 -0500

    initial commit for week 2

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour {
+
+	public GameObject myObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myObj.transform.position = new Vector3(5,1,1);
+	}
+}
